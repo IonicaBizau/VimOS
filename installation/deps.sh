b38a28a8e53c851ed53c8a138816f68bf7e2040b
Ionică Bizău <bizauionica@yahoo.com>
2014-02-09 15:57:27 +0200

    Install dependencies.

---
+++ installation/deps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# screen
+if ! which screen > /dev/null; then
+    echo "> Installing screen ..."
+    sudo apt-get install screen -y
+fi
+
+# elinks
+if ! which elinks > /dev/null; then
+    echo "> Installing elinks ..."
+    sudo apt-get install elinks -y
+fi
+
+# TODO Other apps
--- installation/main.sh
+++ installation/main.sh
@@ -1,5 +1,8 @@
-echo "Copying xsession file ..."
+echo "> Copying xsession file ..."
 sudo cp ./usr/share/xsessions/vimos.desktop /usr/share/xsessions/
 
-echo "Copying executable file ..."
+echo "> Copying executable file ..."
 sudo cp ./usr/bin/vimos /usr/bin
+
+echo "> Installing default applications ..."
+sh ./installation/deps.sh
